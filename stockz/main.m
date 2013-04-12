54e6d01f21c94c38f0e729bc2aeab9b98534c8da
Daniel Buckle <danielbuckle@Daniels-MacBook-Pro.local>
2013-04-12 17:36:51 +0100

    Initial Commit

---
+++ stockz/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  stockz
+//
+//  Created by Daniel Buckle on 12/04/2013.
+//  Copyright (c) 2013 Daniel Buckle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
