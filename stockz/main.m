d265c9bed5a738cddf606d4e521809740f00df82
Daniel Buckle <danielbuckle31@gmail.com>
2013-04-22 12:02:11 +0100

    Original Build
    Original build of stockz, an Objective-C exercise using Property Lists

--- stockz/main.m
+++ stockz/main.m
@@ -13,8 +13,45 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSMutableArray *stocks = [[NSMutableArray alloc] init];
+        
+        NSMutableDictionary *stock;
+        
+        // Create date object
+        NSDate *currentDate = [NSDate date];
+        
+        // Create date format
+        NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+        // Set Date Format
+        [dateFormat setDateFormat: @"dd/MM/yyyy"];
+        // Get Date string
+        NSString *dateToday = [dateFormat stringFromDate:currentDate];
+        
+        stock = [NSMutableDictionary dictionary];
+        [stock setObject:dateToday forKey:@"date"];
+        [stock setObject:@"AAPL" forKey:@"symbol"];
+        [stock setObject:[NSNumber numberWithInt:200] forKey:@"shares"];
+        [stocks addObject:stock];
+        
+        stock = [NSMutableDictionary dictionary];
+        [stock setObject:dateToday forKey:@"date"];
+        [stock setObject:@"GOOG" forKey:@"symbol"];
+        [stock setObject:[NSNumber numberWithInt:160] forKey:@"shares"];
+        [stocks addObject:stock];
+        
+        stock = [NSMutableDictionary dictionary];
+        [stock setObject:dateToday forKey:@"date"];
+        [stock setObject:@"ORCL" forKey:@"symbol"];
+        [stock setObject:[NSNumber numberWithInt:160] forKey:@"shares"];
+        [stocks addObject:stock];
+        
+        [stocks writeToFile:@"/tmp/stocks.plist" atomically:YES];
+        
+        NSArray *stockList = [NSArray arrayWithContentsOfFile:@"/tmp/stocks.plist"];
+        
+        for (NSDictionary *d in stockList) {
+            NSLog(@"I have %@ shares of %@ on the %@", [d objectForKey:@"shares"], [d objectForKey:@"symbol"], [d objectForKey:@"date"]);
+        }
         
     }
     return 0;
